ead32e99dbc8c87b40b7269202ba4cf3455204b2
resteve <resteve@zikzakmedia.com>
2013-10-02 19:17:39 +0200

    setup

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@
 #This file is part internetdomain_invoice_standalone module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
-
 from setuptools import setup
 import re
 import os
 import ConfigParser
+
+MODULE = 'internetdomain_invoice_standalone'
+PREFIX = 'trytonzz'
+MODULE2PREFIX = {}
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = ConfigParser.ConfigParser()
 config.readfp(open('tryton.cfg'))
@@ -21,28 +28,32 @@ minor_version = int(minor_version)
 requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
-        requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+        prefix = MODULE2PREFIX.get(dep, 'trytond')
+        requires.append('%s_%s >= %s.%s, < %s.%s' %
+                (prefix, dep, major_version, minor_version,
+                major_version, minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
         (major_version, minor_version, major_version, minor_version + 1))
 
-setup(name='trytonzz_internetdomain_invoice_standalone',
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
+setup(name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
     description='Tryton module to generete invoice lines when create renewals',
-    author='Sergi Almacellas Abellana',
-    author_email='sergi@koolpi.com',
-    url='http://www.koolpi.com',
+    long_description=read('README'),
+    author='Zikzakmedia SL',
+    url='http://www.zikzakmedia.com/',
     download_url="https://bitbucket.org/zikzakmedia/trytond-internetdomain_invoice_standalone",
-    package_dir={'trytond.modules.internetdomain_invoice_standalone': '.'},
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
-        'trytond.modules.internetdomain_invoice_standalone',
-        'trytond.modules.internetdomain_invoice_standalone.tests',
-    ],
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
+        ],
     package_data={
-        'trytond.modules.internetdomain_invoice_standalone': info.get('xml', []) \
-                + info.get('translation', []),
-    },
+        'trytond.modules.%s' % MODULE: (info.get('xml', [])
+            + ['tryton.cfg', 'locale/*.po']),
+        },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -50,22 +61,29 @@ setup(name='trytonzz_internetdomain_invoice_standalone',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'Intended Audience :: Manufacturing',
         'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
+        'Natural Language :: Czech',
+        'Natural Language :: Dutch',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Natural Language :: German',
+        'Natural Language :: Russian',
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Office/Business',
-    ],
+        ],
     license='GPL-3',
     install_requires=requires,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
-    internetdomain_invoice_standalone = trytond.modules.internetdomain_invoice_standalone
-    """,
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
-)
+    tests_require=tests_require,
+    )
