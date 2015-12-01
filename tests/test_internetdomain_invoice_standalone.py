33a477686770c9574eb16c24fea1282275e58535
resteve <resteve@zikzakmedia.com>
2015-12-01 11:09:33 +0100

    Tests

--- tests/test_internetdomain_invoice_standalone.py
+++ tests/test_internetdomain_invoice_standalone.py
@@ -1,31 +1,18 @@
-#!/usr/bin/env python
-# This file is part internetdomain_invoice_standalone module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the internetdomain_invoice_standalone module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class InternentdomainInvoiceStandaloneTestCase(unittest.TestCase):
+class InternetdomainInvoiceStandaloneTestCase(ModuleTestCase):
     'Test Internetdomain Invoice Standalone module'
+    module = 'internetdomain_invoice_standalone'
 
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('internetdomain_invoice_standalone')
-
-    def test0005views(self):
-        'Test views'
-        test_view('internetdomain_invoice_standalone')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        InternentdomainInvoiceStandaloneTestCase))
+        InternetdomainInvoiceStandaloneTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
